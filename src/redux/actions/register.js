150366c1d6099e10a03e14863f2dc3d477195fa3
rider0211 <codemaster9428@gmail.com>
2024-02-14 10:24:58 -0500

    update git ignore

--- src/redux/actions/register.js
+++ src/redux/actions/register.js
@@ -1,26 +1,31 @@
+// Example action creator for register
+// src/redux/actions.js
+
+import axios from 'axios';
 import useJwt from 'utils/jwt/useJwt';
 
 export const register = (param) => async (dispatch) => {
-    try {
-        useJwt
-        .register(param)
-        .then(res => {
-            console.log(res);
-        //   if (res.data.ResponseCode === 0) {
-            // const data = res.data.ResponseResult
-            // dispatch(handleLogin(data))
-        //   } else {
-        //   }
-        })
-        .catch(err => {
-        //   setSnackBarMsg(err.message);
-        //   setSnackBarOpen(true);
-        console.log(err);
-      })
-        // dispatch({ type: 'LOGIN_SUCCESS', payload: response.data.user });
-        // You might want to save the token here
-    } catch (error) {
-        console.log(error);
-        // dispatch({ type: 'LOGIN_FAILURE', payload: error.response.data });
-    }
+    // try {
+    //     useJwt
+    //     .register(param)
+    //     .then(res => {
+    //         console.log(res);
+    //     //   if (res.data.ResponseCode === 0) {
+    //         // const data = res.data.ResponseResult
+    //         // dispatch(handleLogin(data))
+    //     //   } else {
+    //     //   }
+    //     })
+    //     .catch(err => {
+    //     //   setSnackBarMsg(err.message);
+    //     //   setSnackBarOpen(true);
+    //     console.log(err);
+    //   })
+    //     // dispatch({ type: 'LOGIN_SUCCESS', payload: response.data.user });
+    //     // You might want to save the token here
+    // } catch (error) {
+    //     console.log(error);
+    //     // dispatch({ type: 'LOGIN_FAILURE', payload: error.response.data });
+    // }
+    axios.post("http://192.168.40.28:8000/api/v1/auth/register", param).then(res => console.log(res)).catch(err => console.log(err));
 };
