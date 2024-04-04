cc7e1f637eec82519996dd5c4f225de42ba1470f
rider0211 <codemaster9428@gmail.com>
2024-04-04 17:19:15 -0400

    git ignore

--- src/utils/jwt/messageService.js
+++
@@ -1,8 +0,0 @@
-import { Subject } from 'rxjs';
-
-const subject = new Subject();
-
-export const messageService = {
-    sendMessage: message => subject.next({ text: message }),
-    getMessage: () => subject.asObservable()
-};
